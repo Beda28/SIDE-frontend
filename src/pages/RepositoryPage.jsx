c64f574c11561e39644d77a4b5c06621ee6ceaef
Beda28 <beda4150@gmail.com>
2025-09-15 11:54:07 +0900

    feat: get repo list

--- src/pages/RepositoryPage.jsx
+++ src/pages/RepositoryPage.jsx
@@ -1,9 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import AddRepoModal from "../components/AddRepoModal.jsx";
+import axios from "axios";
 
 export default function RepositoryPage() {
   const [showModal, setShowModal] = useState(false);
+  const [username, setusername] = useState('');
+  const [list, setlist] = useState([]);
+
+  const getid = async () => {
+    const res = await axios.get('http://localhost:4184/api/user/getid', { withCredentials: true })
+    setusername(res.data)
+  }
+
+  const getrepo = async () => {
+    const res = await axios.get('http://localhost:4184/api/repo/getrepo', { withCredentials: true })
+    if (res.data){
+      setlist(res.data)
+    } 
+  }
+
+  useEffect(() => {
+    getid()
+    getrepo()
+  }, [])
 
   return (
     <div style={{ padding: "20px" }}>
@@ -13,6 +33,19 @@ export default function RepositoryPage() {
       <Link to="/ide/123">
         <button>IDE</button>
       </Link>
+
+      <p>{username}</p>
+      <div style={{ display: "flex", flexWrap: "wrap"}}>
+        {list.map((value, index) => {
+          return <>
+            <div key={index} style={{ margin: "30px", border: "1px solid black"}}>
+              <p>full_name: {value.full_name}</p>
+              <p>owner: {value.owner}</p>
+              {value.description ? <p>desc: {value.description}</p> : ""}
+            </div>  
+          </>
+        })}
+      </div>
 
       {showModal && <AddRepoModal onClose={() => setShowModal(false)} />}
     </div>
