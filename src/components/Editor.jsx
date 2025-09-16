c31c9692e0e2d0dc1048228f6d777e8c5d05043a
박지후 <yanggang0424@gmail.com>
2025-09-16 19:46:54 +0900

    feat: 확장자별 에디터 language 지정

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,11 +1,53 @@
 import React from "react";
 import Editor from "@monaco-editor/react";
 
+function getLanguageFromExtension(filename = "") {
+  const ext = filename.split(".").pop().toLowerCase();
+
+  switch (ext) {
+    case "js":
+    case "jsx":
+      return "javascript";
+    case "ts":
+    case "tsx":
+      return "typescript";
+    case "py":
+      return "python";
+    case "java":
+      return "java";
+    case "c":
+      return "c";
+    case "cpp":
+    case "cc":
+    case "cxx":
+      return "cpp";
+    case "json":
+      return "json";
+    case "html":
+      return "html";
+    case "css":
+      return "css";
+    case "md":
+      return "markdown";
+    case "sh":
+      return "shell";
+    case "xml":
+      return "xml";
+    case "yaml":
+    case "yml":
+      return "yaml";
+    default:
+      return "plaintext";
+  }
+}
+
 export default function CodeEditor({ file, content, setContent }) {
+  const language = file ? getLanguageFromExtension(file.name) : "plaintext";
+
   return (
     <Editor
       height="100%"
-      defaultLanguage="javascript"
+      language={language}
       value={content}
       onChange={(value) => setContent(value)}
       theme="vs-dark"
