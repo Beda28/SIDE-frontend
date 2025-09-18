cc7095151e4e51ccd129c2eac2e18448f9203a3e
박지후 <yanggang0424@gmail.com>
2025-09-18 09:05:05 +0900

    feat: logout 기능 추가

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -1,17 +1,44 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+
 const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
+const API_BASE = import.meta.env.VITE_API_BASE_URL;
 
 export default function LoginButton() {
+  const [login, setlogin] = useState()
+
+  useEffect(() => {
+    getId()
+  }, [])
+
+  const getId = async () => {
+    const res = await axios.get(`${API_BASE}/api/user/getid`, {
+      withCredentials: true,
+    });
+    if (res.data) setlogin(true)
+    else setlogin(false)
+  }
+
   const loginWithGithub = () => {
     window.location.assign(
       `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&scope=repo`
     );
   };
 
-  return (
-    <button style={styles.button} onClick={loginWithGithub}>
-      GitHub 로그인
-    </button>
-  );
+  const logout = async () => {
+    await axios.get(`${API_BASE}/api/user/logout`, {
+      withCredentials: true,
+    });
+    setlogin(false)
+  }
+
+  return <>
+    {login ? 
+      <button style={styles.button} onClick={logout}>로그아웃</button> :
+      <button style={styles.button} onClick={loginWithGithub}>GitHub 로그인</button>
+    }
+  </>
 }
 
 const styles = {
