8e56bc1117ae96ff7bf18b41a468adc2bf9f030d
박지후 <yanggang0424@gmail.com>
2025-09-19 20:26:34 +0900

    rename: FileTree 파일 이름 변경

---
+++ src/components/FileTreeNode.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+const FileTreeNode = ({ node, onFileSelect, level = 0, activeFile, onAdd }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const isDirectory = node.type === "folder";
+
+  const isSelected = activeFile && node.path === activeFile.path;
+
+  const handleClick = () => {
+    if (isDirectory) {
+        setIsExpanded(!isExpanded);
+        onFileSelect(node);
+    } else {
+        onFileSelect(node);
+    }
+  };
+
+  const nodeStyle = {
+    cursor: "pointer",
+    margin: "3px 0",
+    color: isDirectory ? "#ccc" : "#fff",
+    backgroundColor: isSelected ? "#333" : "transparent",
+    paddingLeft: `${level * 15}px`,
+  };
+
+  return (
+    <div>
+      <p onClick={handleClick} style={nodeStyle}>
+        {isDirectory ? (isExpanded ? "▼ " : "▶ ") : ""}
+        {isDirectory ? "📁 " : "📄 "}
+        {node.name}
+      </p>
+      {isExpanded && isDirectory && node.children && (
+        <div>
+          {node.children.map((childNode) => (
+            <FileTreeNode
+              key={childNode.path}
+              node={childNode}
+              onFileSelect={onFileSelect}
+              level={level + 1}
+              activeFile={activeFile}
+              onAdd={onAdd}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default FileTreeNode;
