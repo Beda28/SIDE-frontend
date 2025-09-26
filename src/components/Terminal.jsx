788565efd4e43e25c237ca74364e24e95cbc4ecf
Beda28 <ozekr2007@gmail.com>
2025-09-26 09:10:14 +0900

    fix: 터미널 출력

--- src/components/Terminal.jsx
+++ src/components/Terminal.jsx
@@ -7,14 +7,7 @@ import "xterm/css/xterm.css";
 const TerminalComponent = (start) => {
   const { id } = useParams();
   const terminalRef = useRef();
-  const term = new Terminal({ cursorBlink: true });
-
-  useEffect(() => {
-    if (start.start){
-      const socket = new WebSocket(`ws://localhost:4184/api/ws/terminal/${id}`);
-      socket.onmessage = (event) => {term.writeln(event.data); console.log(event.data)};
-    }
-  }, [start])
+  let term = new Terminal({ cursorBlink: true });
 
   useEffect(() => {
     const fitAddon = new FitAddon();
@@ -23,16 +16,32 @@ const TerminalComponent = (start) => {
     term.open(terminalRef.current);
     fitAddon.fit();
 
-    // 샘플 출력
-    // term.writeln("test");
-
-    // WebSocket 연결 
-    // const socket = new WebSocket(`ws://localhost:4184/api/ide/terminal/${id}`);
-    // socket.onmessage = (event) => {term.write(event.data); console.log(event.data)};
-    // term.onData((data) => socket.send(data));  // 터미널 제작시 사용
+    if (start.start){
+      term.write("로그 스트리밍을 시작합니다. 잠시만 기다려주세요.")
+      const socket = new WebSocket(`ws://localhost:4184/api/ws/terminal/${id}`);
+      socket.onmessage = (event) => {term.write(event.data); console.log(event.data)};
+    }
 
     return () => term.dispose();
-  }, []);
+  }, [start])
+
+  // useEffect(() => {
+  //   const fitAddon = new FitAddon();
+  //   term.loadAddon(fitAddon);
+
+  //   term.open(terminalRef.current);
+  //   fitAddon.fit();
+
+  //   // 샘플 출력
+  //   term.writeln("test");
+
+  //   // WebSocket 연결 
+  //   // const socket = new WebSocket(`ws://localhost:4184/api/ide/terminal/${id}`);
+  //   // socket.onmessage = (event) => {term.write(event.data); console.log(event.data)};
+  //   // term.onData((data) => socket.send(data));  // 터미널 제작시 사용
+
+  //   return () => term.dispose();
+  // }, []);
 
   return <div ref={terminalRef} style={{ width: "100%", height: "100%" }} />;
 }
